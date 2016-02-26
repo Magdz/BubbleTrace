462c7248e775be58981e62a1b9b519d74904aae8
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2016-02-26 12:42:21 +0200

    Color Bubbles

--- BubbleTrace/Game.xaml.cs
+++ BubbleTrace/Game.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -22,9 +13,27 @@ namespace BubbleTrace
     /// </summary>
     public sealed partial class Game : Page
     {
+        private Color ColorRed = Color.FromArgb(255, 255, 0, 0);
+        private Color ColorBlue = Color.FromArgb(255, 0, 0, 255);
+        private Color ColorEmpty = Color.FromArgb(255, 244, 244, 245);
+        private Color Color;
         public Game()
         {
             this.InitializeComponent();
+            Color = ColorRed;
+        }
+
+        private void Circle_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Ellipse Circle = (Ellipse)sender;
+            SolidColorBrush solidBrush = new SolidColorBrush();
+            if(((SolidColorBrush)(Circle.Fill)).Color == ColorEmpty)
+            {
+                solidBrush.Color = Color;
+                Circle.Fill = solidBrush;
+                if (Color == ColorRed) Color = ColorBlue;
+                else Color = ColorRed;
+            }
         }
     }
 }
